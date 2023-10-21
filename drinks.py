7f18a156bbef235123226a5924d5b11123d0afae
Howard Kam <16825191+howardkam@users.noreply.github.com>
2023-10-21 11:25:50 -1000

    Update drinks.py

--- drinks.py
+++ drinks.py
@@ -17,15 +17,14 @@ st.markdown(
 )
 
 # User input for price
-price = st.number_input("Enter the price:",min_value=1, max_value=None, value=1, step=None)
+price = st.number_input("Enter the price:",min_value=1, max_value=None, value=1, step=0.01)
 
 # User input for total units
 total_units = st.number_input("Enter the total units:", min_value=1, value=1, step=1)
 
 
 # Calculate price per unit
-price_per_unit = price / total_units
-tax_and_bev = (price_per_unit * 1.04712) + 0.06
+tax_and_bev = ((price / total_units) * 1.04712) + 0.06
 
 # Display the result to the user
 st.write(f"Price per unit with beverage fee: ${tax_and_bev:.2f}")
