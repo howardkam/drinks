f740f96dc6d1b98c2fdb1022f96f7bc60c5a3ae8
Howard Kam <16825191+howardkam@users.noreply.github.com>
2023-10-21 10:27:29 -1000

    Update drinks.py

--- drinks.py
+++ drinks.py
@@ -11,14 +11,15 @@ total_units = st.number_input("Enter the total units:", min_value=1, value=1, st
 
 # Calculate price per unit
 price_per_unit = price / total_units
+with_bev_fee = price_per_unit + 0.06
 
 # Display the result to the user
-st.write(f"Price per unit: ${price_per_unit:.2f}")
+st.write(f"Price per unit with beverage fee: ${with_bev_fee:.2f}")
 
 # Optional: Display a message based on the result
-if price_per_unit < 0.1:
-    st.warning("Bargain alert! The price per unit is very low.")
-elif price_per_unit > 10:
-    st.warning("This might be a bit expensive per unit.")
+st.write("Charge $1, your total units profit will be:")
+st.write(total_units * 1) - ((with_bev_fee * total_units))
+st.write("Charge $2, your total units profit will be:")
+st.write(total_units * 1) - ((with_bev_fee * total_units))
 
 # You can add more features and formatting as desired.
