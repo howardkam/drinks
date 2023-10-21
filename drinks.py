6ce79abb478b95a6260094aa9d4d5a345b39e4b9
Howard Kam <16825191+howardkam@users.noreply.github.com>
2023-10-21 10:52:01 -1000

    Update drinks.py

--- drinks.py
+++ drinks.py
@@ -21,8 +21,8 @@ retail_price_options = [1, 1.5, 2]
 retail_price = st.selectbox("Suggested Retail Price:", retail_price_options)
 
 # Optional: Display a message based on the result
-st.write(f"Total profit @ ${retail_price} dolars:")
-st.write(f"{(total_units * retail_price) - (tax_and_bev * total_units):.2f}")
+st.subheader(f"Total profit @ ${retail_price} dolars:")
+st.subheader(f"{(total_units * retail_price) - (tax_and_bev * total_units):.2f}")
 
 
 
