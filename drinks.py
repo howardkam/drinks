f440c50463440e0eefe0c43062ae7718dab18483
Howard Kam <16825191+howardkam@users.noreply.github.com>
2023-10-21 11:07:03 -1000

    Update drinks.py

--- drinks.py
+++ drinks.py
@@ -22,20 +22,25 @@ price = st.number_input("Enter the price:",min_value=None, max_value=None, value
 # User input for total units
 total_units = st.number_input("Enter the total units:", min_value=1, value=1, step=1)
 
-# Calculate price per unit
-price_per_unit = price / total_units
-tax_and_bev = (price_per_unit * 1.04712) + 0.06
+def calc():
+    # Calculate price per unit
+    price_per_unit = price / total_units
+    tax_and_bev = (price_per_unit * 1.04712) + 0.06
 
-# Display the result to the user
-st.write(f"Price per unit with beverage fee: ${tax_and_bev:.2f}")
+    # Display the result to the user
+    st.write(f"Price per unit with beverage fee: ${tax_and_bev:.2f}")
 
-# Select retail price
-retail_price_options = [1, 1.50, 2]
-retail_price = st.selectbox("Suggested Retail Price:", retail_price_options)
+    # Select retail price
+    retail_price_options = [1, 1.50, 2]
+    retail_price = st.selectbox("Suggested Retail Price:", retail_price_options)
 
-# Optional: Display a message based on the result
-st.subheader(f"Total profit @ ${retail_price}:")
-st.subheader(f"{(total_units * retail_price) - (tax_and_bev * total_units):.2f}")
+    # Optional: Display a message based on the result
+    st.subheader(f"Total profit @ ${retail_price}:")
+    st.subheader(f"{(total_units * retail_price) - (tax_and_bev * total_units):.2f}")
+
+    
+if st.button("Calculate"):
+    calc()
 
 
 
