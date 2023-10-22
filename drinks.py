b1622c35fa86a94e5559e14fc6d3b67b1f076d09
Howard Kam <16825191+howardkam@users.noreply.github.com>
2023-10-21 18:48:44 -1000

    Update drinks.py

--- drinks.py
+++ drinks.py
@@ -66,20 +66,20 @@ with col2:
         unsafe_allow_html=True
     )
     # User input for price
-    price = st.number_input("Enter the price:", min_value=0.01, value=1.0, step=0.01)
+    foodprice = st.number_input("Enter the price:", min_value=0.01, value=1.0, step=0.01)
     # User input for total units
-    total_units = st.number_input("Enter the total units:", min_value=1, value=1, step=1)
+    foodtotal_units = st.number_input("Enter the total units:", min_value=1, value=1, step=1)
     # Calculate cost per unit
     tax = ((price / total_units) * 1.04712)
     st.write(f"Cost per unit after tax and bev fee: ${tax:.2f}")
     # Select retail price per unit
     #retail_price_options = [1, 1.25, 1.50, 2]
     #retail_price = st.selectbox("Suggested Retail Price:", retail_price_options)
-    pre_retail = st.slider("Sell for 1-5", 1.0, 3.0, 1.0, 0.25)
-    retail_price = float(pre_retail)
+    foodpre_retail = st.slider("Sell for 1-5", 1.0, 3.0, 1.0, 0.25)
+    foodretail_price = float(foodpre_retail)
     # Optional: Display a message based on the result
-    st.subheader(f"Total profit @ ${retail_price}:")
-    st.subheader(f"{((retail_price - tax) * total_units):.2f}")
+    st.subheader(f"Total profit @ ${foodretail_price}:")
+    st.subheader(f"{((foodretail_price - tax) * foodtotal_units):.2f}")
     # # Define the content for the fixed footer
     footer_content = """
     <div style="position: fixed; bottom: 0; left: 0; right: 0; background-color: black; text-align: center; padding: 10px;">
