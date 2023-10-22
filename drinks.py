2c29630ae9e2d2824bb5d1aa8b1a6e47e0d1078b
Howard Kam <16825191+howardkam@users.noreply.github.com>
2023-10-21 16:39:15 -1000

    Update drinks.py

--- drinks.py
+++ drinks.py
@@ -30,7 +30,8 @@ st.write(f"Cost per unit after tax and bev fee: ${tax_and_bev:.2f}")
 
 # Select retail price per unit
 retail_price_options = [1, 1.25, 1.50, 2]
-retail_price = st.selectbox("Suggested Retail Price:", retail_price_options)
+# retail_price = st.selectbox("Suggested Retail Price:", retail_price_options)
+retail_price = st.slider("Sell for:", 1, 10.0, step=0.25, value=1.0)
 
 # Optional: Display a message based on the result
 st.subheader(f"Total profit @ ${retail_price}:")
